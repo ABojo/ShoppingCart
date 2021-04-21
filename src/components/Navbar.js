c7b08324b0ae901f3274716967fcee8af64fbe6b
ABojo <alex.bojorquez2598@gmail.com>
2021-04-21 18:34:20 -0400

    Finished navbar component/styling

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -5,9 +5,19 @@ function Navbar() {
     <div className="navbar">
       <h1 className="nav-heading">Shop Name</h1>
       <div className="nav-items">
-        <h1 className="nav-item">Home</h1>
-        <h1 className="nav-item">Shop</h1>
-        <h1 className="nav-item">About</h1>
+        <h1 className="nav-item">
+          <i className="fas fa-home nav-item__icon"></i>Home
+        </h1>
+        <h1 className="nav-item">
+          <i className="fas fa-shopping-bag nav-item__icon"></i>Shop
+        </h1>
+        <h1 className="nav-item">
+          <i className="fas fa-user  nav-item__icon"></i>About
+        </h1>
+        <h1 className="nav-cart">
+          <p className="nav-cart__count">0</p>
+          <i className="fas fa-shopping-cart"></i>
+        </h1>
       </div>
     </div>
   );
