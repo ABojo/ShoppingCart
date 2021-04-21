d275d75808ef8c07a30e1d819e978b1af420e66d
ABojo <alex.bojorquez2598@gmail.com>
2021-04-21 17:21:51 -0400

    Continued building and styling navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,10 +3,11 @@ import styles from '../styles/navStyles.css';
 function Navbar() {
   return (
     <div className="navbar">
-      <h1>Shop Name</h1>
-      <div className="navitems-box">
-        <h1>Home</h1>
-        <h1>Shop</h1>
+      <h1 className="nav-heading">Shop Name</h1>
+      <div className="nav-items">
+        <h1 className="nav-item">Home</h1>
+        <h1 className="nav-item">Shop</h1>
+        <h1 className="nav-item">About</h1>
       </div>
     </div>
   );
