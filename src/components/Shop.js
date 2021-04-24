f0a33b180b3314c02e494651c62456bb30b26365
ABojo <alex.bojorquez2598@gmail.com>
2021-04-24 01:02:48 -0400

    Now rendering grid based on the list passed in as props and cleaned up imports

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -1,54 +1,31 @@
 import React from 'react';
 import styles from '../styles/shopStyles.css';
-import FireRed from '../img/firered.jpg';
-import halo from '../img/halo.png';
-import bo2 from '../img/bo2.jpg';
-import ssb from '../img/ssb.jpg';
 
 function Shop(props) {
-  console.log(props);
+  const { products } = props;
   return (
     <React.Fragment>
       <h1 className="shop-heading">Shop</h1>
-
       <div className="shop-grid">
-        <div className="shop-item">
-          <img
-            className="shop-item__image"
-            alt={'fire red'}
-            src={FireRed}
-          ></img>
-          <h1 className="shop-item__name">Pokemon Fire Red</h1>
-          <p className="shop-item__price">$29.99</p>
-          <button className="shop-item__atc">Add To Cart</button>
-          <input
-            className="shop-item__quantity"
-            type="number"
-            defaultValue="1"
-          />
-        </div>
-        <div className="shop-item">
-          <img className="shop-item__image" alt={'halo'} src={halo}></img>
-          <h1 className="shop-item__name">Halo Infinite</h1>
-          <p className="shop-item__price">$69.99</p>
-          <button className="shop-item__atc">Add To Cart</button>
-          <input
-            className="shop-item__quantity"
-            type="number"
-            defaultValue="1"
-          />
-        </div>
-        <div className="shop-item">
-          <img className="shop-item__image" alt={'fire red'} src={bo2}></img>
-          <h1 className="shop-item__name">CoD: Black Ops 2</h1>
-          <p className="shop-item__price">$59.99</p>
-          <button className="shop-item__atc">Add To Cart</button>
-          <input
-            className="shop-item__quantity"
-            type="number"
-            defaultValue="1"
-          />
-        </div>
+        {products.map((product) => {
+          return (
+            <div className="shop-item">
+              <img
+                className="shop-item__image"
+                alt={product.name}
+                src={product.image}
+              ></img>
+              <h1 className="shop-item__name">{product.name}</h1>
+              <p className="shop-item__price">${product.price}</p>
+              <button className="shop-item__atc">Add To Cart</button>
+              <input
+                className="shop-item__quantity"
+                type="number"
+                defaultValue="1"
+              />
+            </div>
+          );
+        })}
       </div>
     </React.Fragment>
   );
