f36994dff6988be58ba8281cfeba7b48f02b2150
ABojo <alex.bojorquez2598@gmail.com>
2021-04-24 00:38:20 -0400

    updated styling + added an input for setting quantity

--- src/components/Shop.js
+++ src/components/Shop.js
@@ -8,7 +8,7 @@ import ssb from '../img/ssb.jpg';
 function Home(props) {
   return (
     <React.Fragment>
-      <h1 className="shop-heading">Generic Shop</h1>
+      <h1 className="shop-heading">Shop</h1>
 
       <div className="shop-grid">
         <div className="shop-item">
@@ -20,18 +20,33 @@ function Home(props) {
           <h1 className="shop-item__name">Pokemon Fire Red</h1>
           <p className="shop-item__price">$29.99</p>
           <button className="shop-item__atc">Add To Cart</button>
+          <input
+            className="shop-item__quantity"
+            type="number"
+            defaultValue="1"
+          />
         </div>
         <div className="shop-item">
           <img className="shop-item__image" alt={'halo'} src={halo}></img>
           <h1 className="shop-item__name">Halo Infinite</h1>
           <p className="shop-item__price">$69.99</p>
           <button className="shop-item__atc">Add To Cart</button>
+          <input
+            className="shop-item__quantity"
+            type="number"
+            defaultValue="1"
+          />
         </div>
         <div className="shop-item">
           <img className="shop-item__image" alt={'fire red'} src={bo2}></img>
           <h1 className="shop-item__name">CoD: Black Ops 2</h1>
           <p className="shop-item__price">$59.99</p>
           <button className="shop-item__atc">Add To Cart</button>
+          <input
+            className="shop-item__quantity"
+            type="number"
+            defaultValue="1"
+          />
         </div>
       </div>
     </React.Fragment>
